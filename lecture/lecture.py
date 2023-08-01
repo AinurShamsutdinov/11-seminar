4cbae9e2f28e9edfc7d7ff128f5ad507d7d9e24c
Ainur Shamsutdinov <shamsut@gmail.com>
2023-08-01 18:54:19 +0300

    lecture

--- lecture/lecture.py
+++ lecture/lecture.py
@@ -305,27 +305,124 @@
 #     storeroom = storeroom >> 3
 #     print(storeroom)
 ########################################################################################################
-class StrPro(str):
-
-    def __new__(cls, *args, **kwargs):
-        instance = super().__new__(cls, *args, **kwargs)
-        return instance
-
-    def __rmul__(self, other: str):
-        words = other.split()
-        result = self.join(words) + ' test '
-        return StrPro(result)
-
-
-text = 'Каждый охотник желает знать где сидит фазан'
-s = StrPro(' (=^.^=) ')
-print(f'{text = }\n{s = }')
-print(text * s)
-# print(s * text)     # TypeError: 'str' object cannot be interpreted as an integer
+# class StrPro(str):
+#
+#     def __new__(cls, *args, **kwargs):
+#         instance = super().__new__(cls, *args, **kwargs)
+#         return instance
+#
+#     def __rmul__(self, other: str):
+#         words = other.split()
+#         result = self.join(words) + ' test '
+#         return StrPro(result)
+#
+#
+# text = 'Каждый охотник желает знать где сидит фазан'
+# s = StrPro(' (=^.^=) ')
+# print(f'{text = }\n{s = }')
+# print(text * s)
+# # print(s * text)     # TypeError: 'str' object cannot be interpreted as an integer
 ########################################################################################################
+# from decimal import Decimal
+#
+# class Money:
+#     def __init__(self, value: int | float):
+#         self.value = Decimal(value)
+#
+#     def __repr__(self):
+#         return f'Money({self.value:.2f})'
+#
+#     def __imod__(self, other):
+#         self.value = self.value * Decimal(1 + other / 100)
+#         return self
+#
+#
+# m = Money(100)
+# print(m)
+# m %= 50
+# print(m)
+# m %= 100
+# print(m)
 ########################################################################################################
+# class Triangle:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def __str__(self):
+#         return f'Треугольник со сторонами: {self.a}, {self.b}, {self.c}'
+#
+#
+# one = Triangle(3, 4, 5)
+# two = one
+# three = Triangle(3, 4, 5)
+# print(one == two)
+# print(one == three)
 ########################################################################################################
+# class Triangle:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def __str__(self):
+#         return f'Треугольник со сторонами: {self.a}, {self.b}, {self.c}'
+#
+#     def __eq__(self, other):
+#         first = sorted((self.a, self.b, self.c))
+#         second = sorted((other.a, other.b, other.c))
+#         return first == second
+#
+#
+# one = Triangle(3, 4, 5)
+# two = one
+# three = Triangle(3, 4, 5)
+# four = Triangle(4, 3, 5)
+# print(f'{one == two   = }')
+# print(f'{one == three = }')
+# print(f'{one == four  = }')
+# print(f'{one != one   = }')
 ########################################################################################################
+from math import sqrt
+
+
+class Triangle:
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def __str__(self):
+        return f'Треугольник со сторонами: {self.a}, {self.b}, {self.c}'
+
+    def __repr__(self):
+        return f'Triangle({self.a}, {self.b}, {self.c})'
+
+    def __eq__(self, other):
+        first = sorted((self.a, self.b, self.c))
+        second = sorted((other.a, other.b, other.c))
+        return first == second
+
+    def area(self):
+        p = (self.a + self.b + self.c) / 2
+        _area = sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
+        return _area
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+
+one = Triangle(3, 4, 5)
+two = Triangle(5, 5, 5)
+print(f'{one} имеет площадь {one.area():.3f} у.е.2')
+print(f'{two} имеет площадь {two.area():.3f} у.е.2')
+print(f'{one > two  = }\n{one < two  = }')
+data = [Triangle(3, 4, 5), Triangle(6, 2, 5), Triangle(4, 4, 4), Triangle(3, 5, 3)]
+result = sorted(data)
+print(result)
+print(', '.join(f'{item.area():.3f}' for item in result))
 ########################################################################################################
 ########################################################################################################
 ########################################################################################################
