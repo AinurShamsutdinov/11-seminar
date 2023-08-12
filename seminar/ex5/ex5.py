fe5465e071c89cc2f3e9c0a96ecc4ac8f6faa750
Ainur Shamsutdinov <shamsut@gmail.com>
2023-08-13 00:23:57 +0300

    7th ex solution, documentation and formatted string representation

--- seminar/ex5/ex5.py
+++ seminar/ex5/ex5.py
@@ -7,10 +7,12 @@
 
 
 class Rectangle(object):
+    """Class to represent a rectangle"""
     length: int
     depth: int
 
     def __init__(self, length, depth):
+        """Initiate a rectangle"""
         if (length is None) or (depth is None):
             if length is not None:
                 self.length = length
@@ -23,17 +25,21 @@ class Rectangle(object):
             self.depth = depth
 
     def perimeter(self):
+        """Calculate the perimeter of a rectangle"""
         return (self.length + self.depth) * 2
 
     def area(self):
+        """Calculate the area of a rectangle"""
         return self.length * self.depth
 
     def __add__(self, other):
+        """Sum perimeters of two rectangles and get a new rectangle"""
         sum_perimeters = self.perimeter() + other.perimeter()
         sum_sides = sum_perimeters / 2
         return Rectangle(int(sum_sides / 2), int(sum_sides / 2))
 
     def __sub__(self, other):
+        """Substuct perimeters if the result is bigger than 0 and return new Rectangle, else return None"""
         sub_perimeters: int
         sub_sides: int
         if self.perimeter() > other.perimeter():
@@ -41,6 +47,14 @@ class Rectangle(object):
             sub_sides = sub_perimeters / 2
             return Rectangle(int(sub_sides / 2), int(sub_sides / 2))
         return None
+
+    def __str__(self):
+        """Get the string representation of the object"""
+        return f'depth = {self.depth} length = {self.length}'
+
+    def __repr__(self):
+        """Get the formatted string representation of the object"""
+        return f'Rectangle({self.depth}, {self.length})'
 
 
 first = Rectangle(10, 20)
@@ -51,4 +65,8 @@ sub = second - first
 
 print(summ.depth, summ.length)
 print(sub.depth, sub.length)
+print(summ)
+print(sub)
+print(f'{summ = }')
+print(f'{sub = }')
 
--- seminar/ex6/ex6.py
+++ seminar/ex6/ex6.py
@@ -5,10 +5,12 @@
 
 
 class Rectangle(object):
+    """Class to represent a rectangle"""
     length: int
     depth: int
 
     def __init__(self, length, depth):
+        """Initiate a rectangle"""
         if (length is None) or (depth is None):
             if length is not None:
                 self.length = length
@@ -21,17 +23,21 @@ class Rectangle(object):
             self.depth = depth
 
     def perimeter(self):
+        """Calculate the perimeter of a rectangle"""
         return (self.length + self.depth) * 2
 
     def area(self):
+        """Calculate the area of a rectangle"""
         return self.length * self.depth
 
     def __add__(self, other):
+        """Sum perimeters of two rectangles and get a new rectangle"""
         sum_perimeters = self.perimeter() + other.perimeter()
         sum_sides = sum_perimeters / 2
         return Rectangle(int(sum_sides / 2), int(sum_sides / 2))
 
     def __sub__(self, other):
+        """Substuct perimeters if the result is bigger than 0 and return new Rectangle, else return None"""
         sub_perimeters: int
         sub_sides: int
         if self.perimeter() > other.perimeter():
@@ -40,32 +46,46 @@ class Rectangle(object):
             return Rectangle(int(sub_sides / 2), int(sub_sides / 2))
         return None
 
+    def __str__(self):
+        """Get the string representation of the object"""
+        return f'depth = {self.depth} length = {self.length}'
+
+    def __repr__(self):
+        """Get the formatted string representation of the object"""
+        return f'Rectangle({self.depth}, {self.length})'
+
     def __eq__(self, other):
+        """Check if two rectangles are equal"""
         first = self.area()
         second = other.area()
         return first == second
 
     def __ne__(self, other):
+        """Check if two rectangles are not equal"""
         first = self.area()
         second = other.area()
         return first != second
 
     def __gt__(self, other):
+        """Check if rectangle is bigger than another"""
         first = self.area()
         second = other.area()
         return first > second
 
     def __le__(self, other):
+        """Check if rectangle is smaller or equal to another"""
         first = self.area()
         second = other.area()
         return first <= second
 
     def __lt__(self, other):
+        """Check if rectange is smaller than another"""
         first = self.area()
         second = other.area()
         return first < second
 
     def __ge__(self, other):
+        """Check if rectangle is bigger of equal to another"""
         first = self.area()
         second = other.area()
         return first >= second
@@ -86,3 +106,7 @@ print('first < second ', first_rec < second_rec)
 print('first < third ', first_rec < third_rec)
 print('first >= second ', first_rec >= second_rec)
 print('third >= second ', third_rec >= second_rec)
+
+print(f'{first_rec = }')
+print(f'{second_rec = }')
+print(f'{third_rec = }')
