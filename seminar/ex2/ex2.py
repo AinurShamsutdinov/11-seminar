5c7dd8447d5c306815e0f949408ad4a43fd42969
Ainur Shamsutdinov <shamsut@gmail.com>
2023-08-12 18:59:22 +0300

    2nd ex seminar

--- seminar/ex2/ex2.py
+++ seminar/ex2/ex2.py
@@ -5,26 +5,53 @@
 
 
 class Archive:
+    _old_instance = None
     __number = 0
     __some_string = ''
-    lst_numbers = list()
-    lst_some_strings = list()
+    __lst_numbers = list()
+    __lst_some_strings = list()
 
-    def __new__(cls, number, some_str):
-        cls.lst_numbers.append(number)
-        cls.lst_some_strings.append(some_str)
-        cls.__number = number
-        cls.__some_string = some_str
+    def __new__(cls, *args):
+        instance = super().__new__(cls)
+        instance.__number = args[0]
+        instance.__some_string = args[1]
+        if cls._old_instance is None:
+            cls._old_instance = instance
+        elif cls._old_instance is not None:
+            if cls._old_instance.__lst_numbers is not None:
+                instance.__lst_numbers = list(cls._old_instance.__lst_numbers)
+            else:
+                instance.__lst_numbers = list()
+            if cls._old_instance.__lst_some_strings is not None:
+                instance.__lst_some_strings = list(cls._old_instance.__lst_some_strings)
+            else:
+                instance.__lst_some_strings = list()
+            instance.__lst_numbers.append(cls._old_instance.__number)
+            instance.__lst_some_strings.append(cls._old_instance.__some_string)
+            cls.__old_instance = instance
+        return instance
 
-    def __init__(self, number, some_str):
-        self.__number = number
-        self.__some_string = some_str
+    def __str__(self):
+        return self.__number, self.__some_string
 
-    def get_archive(self):
-        return self.lst_numbers, self.lst_some_strings
+    def get_lst_numbers(self):
+        return self.__lst_numbers
+
+    def get_lst_some_strings(self):
+        return self.__lst_some_strings
+
+    def get_number(self):
+        return self.__number
+
+    def get_some_string(self):
+        return self.__lst_some_strings
 
 
-archive = Archive(10, '10')
-archive = Archive(20, '20')
+archive = Archive(10, '11')
+archive = Archive(20, '22')
+archive = Archive(30, '33')
+archive = Archive(40, '44')
 
-print(archive.lst_numbers)
+print(f'{archive = }', type(archive))
+print(f'{archive.get_lst_numbers() = }')
+print(f'{archive.get_lst_some_strings() = }')
